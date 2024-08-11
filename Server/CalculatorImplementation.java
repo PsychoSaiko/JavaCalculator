61439207ce3a2d1a12c5a4415cd3b3072081d142
Karanbir Singh <karanbir8811@gmail.com>
2024-08-11 22:55:03 +0930

    Calculator Implementation

--- Server/CalculatorImplementation.java
+++ Server/CalculatorImplementation.java
@@ -7,8 +7,6 @@ public class CalculatorImplementation extends UnicastRemoteObject implements Cal
     super();
     stack = new Stack<>();
 }
-
-
 public void pushValue(int val) {
     stack.push(val);
 }
@@ -18,27 +16,57 @@ public void pushOperation(String operation){
         throw new IllegalArgumentException("Invalid Oper" + operator);
     }
     int result=0;
-    if(operation=="min"){
-        result = minimumOperation(stack);
-    } else if (operation=="max") {
-        result = minimumOperation(stack);
-    } else if (operation=="lcm") {
-        result = minimumOperation(stack);
-    } else if (operation=="gcd") {
-        result = minimumOperation(stack);
-    }else
-    {
-        throw new IllegalArgumentException("Invalid Oper" + operator);
-    }
+    if(operation=="min"){result = minimumOperation(stack);}
+    else if (operation=="max") {result = maximumOperation(stack);}
+    else if (operation=="lcm") {result = lcmOperation(stack);}
+    else if (operation=="gcd") {result = gcdOperation(stack);}
+    else {throw new IllegalArgumentException("Invalid Oper" + operator);}
     stack.clear();
     stack.push(result);
-
-
 }
-private int minimumOperation(Stack<Integer> st)
-{
+    private int minimumOperation(Stack<Integer> st) {
+        int minVal;
+        minVal=st.pop();
+        while(!st.isEmpty()) {
+            minVal=Math.min(minVal,st.pop());
+        }
+        return minVal;
+    }
+    private int maximumOperation(Stack<Integer> st) {
+        int maxVal;
+        maxVal=st.pop();
+        while(!st.isEmpty()) {
+            maxVal=Math.max(maxVal,st.pop());
+        }
+        return maxVal;
+    }
+    private int lcmOperation(Stack<Integer> st) {
+        int lcmValue, poppedEle;
+        lcmValue = st.pop();
+        if (st.isEmpty()) {
+            return  lcmValue;
+        }
+        while(!st.isEmpty()) {
+            poppedEle=st.pop();
+            lcmValue=(lcmValue*poppedEle)/gcdCalculation(lcmValue,poppedEle);
 
-}
-
+        }
+        return lcmValue;
+    }
+    private int gcdOperation(Stack<Integer> st) {
+        int gcdValue, poppedEle;
+        gcdValue = st.pop();
+        if (st.isEmpty()) {
+            return  gcdValue;
+        }
+        while(!st.isEmpty()) {
+            poppedEle=st.pop();
+            gcdValue=gcdCalculation(gcdValue,poppedEle);}
+        return gcdValue;
+    }
+    private int gcdCalculation(int a ,int b) {
+        if (b == 0) return a;
+        return gcdCalculation(b, a%b);
+    }
 
 }
